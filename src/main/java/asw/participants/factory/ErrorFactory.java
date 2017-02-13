b5c0ea3e2dc360eef12520d9ffd7616df08cab59
Martin <martinpelaezdiaz@gmail.com>
2017-02-13 18:39:49 +0100

    Crear error unknown, y implementacion de la factoria

--- src/main/java/asw/participants/factory/ErrorFactory.java
+++ src/main/java/asw/participants/factory/ErrorFactory.java
@@ -1,6 +1,40 @@
 package asw.participants.factory;
 
+import asw.participants.webService.responses.errors.ErrorResponse;
+import asw.participants.webService.responses.errors.IncorrectPasswordResponse;
+import asw.participants.webService.responses.errors.RequiredEmailErrorResponse;
+import asw.participants.webService.responses.errors.RequiredPasswordErrorResponse;
+import asw.participants.webService.responses.errors.UnknownErrorResponse;
+import asw.participants.webService.responses.errors.UserNotFoundResponse;
+
 //Creacion de los distintos tipos de error.
 public class ErrorFactory {
+
+	public static enum Errors {
+		INCORRECT_PASSWORD,
+		REQUIRED_EMAIL,
+		REQUIRED_PASSWORD,
+		USER_NOT_FOUND
+	}
+
+	// Generar Constructor privado no queremos que se pueda tener varias
+	// instancias de la clase.
+	private ErrorFactory() {
+	}
+
+	public static ErrorResponse getError(Errors error) {
+		switch (error) {
+		case INCORRECT_PASSWORD:
+			return new IncorrectPasswordResponse();
+		case REQUIRED_EMAIL:
+			return new RequiredEmailErrorResponse();
+		case REQUIRED_PASSWORD:
+			return new RequiredPasswordErrorResponse();
+		case USER_NOT_FOUND:
+			return new UserNotFoundResponse();
+		default:// en caso de no conocer el error.
+			return new UnknownErrorResponse();
+		}
+	}
 
 }
