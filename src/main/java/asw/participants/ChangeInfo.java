be5c789df80109c86db6cd3b8f2a77235ec5dedf
LauraMenendez <UO244646@uniovi.es>
2017-02-18 19:25:03 +0100

    Merge y comentarios.

--- src/main/java/asw/participants/ChangeInfo.java
+++ src/main/java/asw/participants/ChangeInfo.java
@@ -4,7 +4,19 @@ import asw.participants.webService.request.PeticionChangeEmailREST;
 import asw.participants.webService.request.PeticionChangePasswordREST;
 
 public interface ChangeInfo {
+	/**
+	 * Cambio de contraseña
+	 * @param accept header, puede ser xml o json, en funcion respondera una cosa u otra
+	 * @param datos requeridos
+	 * @return respuesta en xml o json
+	 */
 	public Object changePassword(String accept, PeticionChangePasswordREST datos);
 
+	/**
+	 * Cambio de email
+	 * @param accept header, puede ser xml o json, en funcion respondera una cosa u otra
+	 * @param datos requeridos
+	 * @return respuesta en xml o json
+	 */
 	public Object changeEmail(String accept, PeticionChangeEmailREST datos);
 }
